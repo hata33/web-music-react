a0ae1f4e482a9c11fead1a0c46fd9d2bcf21df3d
zhaowenzhe <1553126902@qq.com>
2021-10-15 22:44:48 +0800

    test

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from './reducer';
 
-// 让浏览器中的redux dev-tool 起作用
+// 让浏览器中的redux dev-tool起作用
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(reducer, composeEnhancers(
